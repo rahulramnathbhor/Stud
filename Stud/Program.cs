019409d2b12cf4f258cb8394cce60bfd53aefbf2
rahulramnathbhor <112869809+rahulramnathbhor@users.noreply.github.com>
2022-09-15 16:25:12 +0530

    Add project files.

---
+++ Stud/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Stud.Student;
+
+namespace Stud
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            {
+
+                Student s = new Student();
+                s.Rollno = 1;
+                s.Name = "Rahul";
+                s.Percentage = 89.00;
+                Console.WriteLine($"Rollno {s.Rollno} {s.Name}{s.Percentage}");
+
+            }
+        }
+    }
+}
---
+++ Stud/Student.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stud
+{
+    public class Student
+    {
+        
+        
+            private int rollno;
+            private string name;
+            private double perc;
+
+          
+            public int Rollno
+            {
+                get { return rollno; }
+                set { rollno = value; }
+            }
+            public string Name
+            {
+                get { return name; }
+                set { name = value; }
+            }
+            public double Percentage
+            {
+                get { return perc; }
+                set { perc = value; }
+            }
+        
+
+    }
+}
